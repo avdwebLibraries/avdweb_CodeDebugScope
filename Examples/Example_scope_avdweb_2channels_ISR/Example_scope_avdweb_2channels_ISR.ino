15ddff0c87f3a1e4202c917152def275c4f6e68e
Albert van Dalen <albertfora@avdweb.nl>
2024-02-20 15:47:42 +0100

    Add files via upload

---
+++ Examples/Example_scope_avdweb_2channels_ISR/Example_scope_avdweb_2channels_ISR.ino
@@ -0,0 +1,25 @@
+#include "avdweb_SWscope.h"
+#include <FrequencyTimer2.h> // mainISR https://github.com/PaulStoffregen/FrequencyTimer2
+
+// Attention: set in avdweb_SWscope.h: const int maxScopeBytes = 100; // or more if possible
+
+SWscope scope;
+
+void setup(void)
+{ Serial.begin(115200);
+  FrequencyTimer2::setPeriod(250); // period = 250us
+  FrequencyTimer2::setOnOverflow(mainISR); // start ISR here
+  scope.start(2, 10); // 2 integer channels , preSamples = 10, buffer is 100/4=25 integer values
+  scope.trigger();
+}
+
+void mainISR(void)
+{ static int internalValue;
+  internalValue += 2;
+  scope.probeAB(internalValue, analogRead(A0)); // channel A = internalValue, channel B = ADC
+  if(internalValue > 10) scope.trigger();
+}
+
+void loop(void)
+{ scope.showIfReady(); 
+}
